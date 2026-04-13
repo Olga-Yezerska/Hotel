7196fe061807dc5f1d24e798e6ea0cbd55c278d5
Olga Yezerska <olga_ezerska06@ukr.net>
2026-04-13 13:27:09 +0300

    feat: add category table

--- hotel/models.py
+++ hotel/models.py
@@ -8,3 +8,10 @@ class Amenity(models.Model):
     def __str__(self):
         return self.name
 
+class Category(models.Model):
+    name = models.CharField(max_length = 100)
+    description = models.TextField(blank = True)
+
+    def __str__(self):
+        return self.name
+    
