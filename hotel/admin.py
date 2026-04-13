b8a1546d83831d1b610aa4e0b0a223acb1a28a81
Olga Yezerska <olga_ezerska06@ukr.net>
2026-04-13 13:28:45 +0300

    feat: add models to admin file

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Amenity, Category, Room, HotelInfo
+
+admin.site.register(Amenity)
+admin.site.register(Category)
+admin.site.register(Room)
+admin.site.register(HotelInfo)
