0a3f263dcc8e5f0370867d5fb5e278114d8c7541
anna <lia945785l@gmail.com>
2026-05-04 16:28:56 +0300

    feat: update create_booking logic and price calculation

--- hotel/views.py
+++ hotel/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from datetime import datetime
 from .models import Room, Booking, Category
 from hotel.filters import RoomFilter
@@ -54,6 +54,14 @@ def room_details(request, pk):
         'categories': Category.objects.all()
     })
 
+def room_details_booking(request, pk):
+    room = get_object_or_404(Room, pk=pk)
+    
+    return render(request, 'hotel/room-details-booking.html', {
+        'room': room,
+        'categories': Category.objects.all()
+    })
+
 def rooms_by_category(request, category_id):
     category = get_object_or_404(Category, pk=category_id)
     rooms = Room.objects.filter(category=category)
@@ -73,4 +81,47 @@ def booking_view(request):
 
 def create_booking(request, pk):
     room = get_object_or_404(Room, pk=pk)
-    return render(request, 'hotel/create_booking.html', {'room': room})
+    check_in_str = request.GET.get('check_in')
+    check_out_str = request.GET.get('check_out')
+
+    # Оголошуємо змінні заздалегідь
+    num_nights = 0
+    total_price = 0
+
+    try:
+        check_in = datetime.strptime(check_in_str, '%d %B, %Y').date()
+        check_out = datetime.strptime(check_out_str, '%d %B, %Y').date()
+        
+        num_nights = (check_out - check_in).days
+        if num_nights < 1: 
+            num_nights = 1
+        
+        total_price = room.price * num_nights
+        
+    except (ValueError, TypeError):
+        return redirect('rooms')
+
+    if request.method == 'POST':
+        customer_name = request.POST.get('customer_name')
+        customer_email = request.POST.get('customer_email')
+        customer_phone = request.POST.get('customer_phone')
+        
+        Booking.objects.create(
+            room=room,
+            check_in=check_in,
+            check_out=check_out,
+            customer_name=customer_name,
+            customer_email=customer_email,
+            customer_phone=customer_phone,
+            guests=room.capacity
+        )
+        return redirect('home')
+
+
+    return render(request, 'hotel/create_booking.html', {
+        'room': room,
+        'check_in': check_in,
+        'check_out': check_out,
+        'num_nights': num_nights,    # Переконайся, що ці рядки є
+        'total_price': total_price,  # Переконайся, що ці рядки є
+    })
